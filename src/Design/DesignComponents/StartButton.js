3400d0a84529f67a2f96fd3ea2a3ef175282d21b
Darshana Perera <darshana.saluka.10@gmail.com>
2022-04-10 17:34:43 +0530

    stylesheet added

--- src/Design/DesignComponents/StartButton.js
+++ src/Design/DesignComponents/StartButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { purple } from '@mui/material/colors';
+import { purple,red } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
@@ -7,15 +7,15 @@ import Button from '@mui/material/Button';
 
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
-  backgroundColor: purple[500],
+  backgroundColor: red[500],
   "&:hover": {
-    backgroundColor: purple[700],
+    backgroundColor: red[900],
   },
 }));
 
 export default function StartButton() {
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       <ColorButton variant="contained">Custom CSS</ColorButton>
     </div>
   );
